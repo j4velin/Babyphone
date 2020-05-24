25ee2e78884217fd4f1462f4f281ecc67745a88d
Thomas Hoffmann <git@j4velin.de>
2020-05-24 19:26:15 +0200

    init

---
+++ app/src/main/java/de/j4velin/babyphone/MainActivity.kt
@@ -0,0 +1,12 @@
+package de.j4velin.babyphone
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
