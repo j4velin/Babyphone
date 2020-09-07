3c40b7e67f757e84d5b2b23c309547dc82a249e7
Thomas Hoffmann <git@j4velin.de>
2020-09-07 14:53:36 +0200

    fix IllegalStateException when UI is also recording

--- app/src/main/java/de/j4velin/babyphone/Recorder.kt
+++ app/src/main/java/de/j4velin/babyphone/Recorder.kt
@@ -1,7 +1,9 @@
 package de.j4velin.babyphone
 
 import android.media.MediaRecorder
+import android.util.Log
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * An object, which can be stopped
@@ -30,6 +32,9 @@ fun getAmplitude(delayMs: Long, action: (Int) -> Unit): Stoppable {
     }
 }
 
+private val recorder: MediaRecorder by lazy { MediaRecorder() }
+private val listener = AtomicInteger(0)
+
 /**
  * Continuously gets the max amplitude between two measurements as long as the condition is fulfilled
  *
@@ -38,20 +43,29 @@ fun getAmplitude(delayMs: Long, action: (Int) -> Unit): Stoppable {
  * @param action the action to invoke when a value was measured
  */
 fun getAmplitudeWhile(condition: () -> Boolean, delayMs: Long, action: (Int) -> Unit) {
-    val mRecorder = MediaRecorder()
-    mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-    mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-    mRecorder.setOutputFile("/dev/null")
-    mRecorder.prepare()
-    mRecorder.start()
-
-    mRecorder.maxAmplitude // ignore first value
+    if (listener.getAndIncrement() == 0) {
+        // first listener? then start recoding
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+        recorder.setOutputFile("/dev/null")
+        recorder.prepare()
+        recorder.start()
+        recorder.maxAmplitude // ignore first value
+        Log.i(TAG, "recoding started")
+    }
 
     Thread {
         while (condition.invoke()) {
-            action.invoke(mRecorder.maxAmplitude)
+            action.invoke(recorder.maxAmplitude)
             Thread.sleep(delayMs)
+        }
+        if (listener.decrementAndGet() == 0) {
+            // no component needs amplitude updates any more -> stop recording
+            recorder.stop()
+            Log.i(TAG, "all listener expired -> recoding stopped")
+        } else {
+            Log.i(TAG, "listener expired but recording still active")
         }
     }.start()
 }
